62288e24b3e79875d3d6e95be406e0a4473bbe34
AliceYangAC <yang0447@algonquinlive.com>
2026-02-03 13:14:53 -0500

    feature: added iac for azure container group

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -2,6 +2,7 @@ import * as pulumi from "@pulumi/pulumi";
 import * as resources from '@pulumi/azure-native/resources'
 import * as containerregistry from '@pulumi/azure-native/containerregistry'
 import * as dockerBuild from '@pulumi/docker-build'
+import * as containerinstance from '@pulumi/azure-native/containerinstance'
 
 // Import the configuration settings for the current stack.
 const config = new pulumi.Config()
@@ -57,5 +58,80 @@ const image = new dockerBuild.Image(`${prefixName}-image`, {
   ],
 })
 
-export const acrServer = registry.loginServer
-export const acrUsername = registryCredentials.username
+const image = new dockerBuild.Image(`${prefixName}-image`, {
+  tags: [pulumi.interpolate`${registry.loginServer}/${imageName}:${imageTag}`],
+  context: { location: appPath },
+  dockerfile: { location: `${appPath}/Dockerfile` },
+  target: 'production',
+  platforms: ['linux/amd64', 'linux/arm64'],
+  push: true,
+  registries: [
+    {
+      address: registry.loginServer,
+      username: registryCredentials.username,
+      password: registryCredentials.password,
+    },
+  ],
+})
+
+// Create a container group in the Azure Container App service and make it publicly accessible.
+const containerGroup = new containerinstance.ContainerGroup(
+  `${prefixName}-container-group`,
+  {
+    resourceGroupName: resourceGroup.name,
+    osType: 'linux',
+    restartPolicy: 'always',
+    imageRegistryCredentials: [
+      {
+        server: registry.loginServer,
+        username: registryCredentials.username,
+        password: registryCredentials.password,
+      },
+    ],
+    containers: [
+      {
+        name: imageName,
+        image: image.ref,
+        ports: [
+          {
+            port: containerPort,
+            protocol: 'tcp',
+          },
+        ],
+        environmentVariables: [
+          {
+            name: 'PORT',
+            value: containerPort.toString(),
+          },
+          {
+            name: 'WEATHER_API_KEY',
+            value: '4e176c068fe925a4e1427342ac5f91ba',
+          },
+        ],
+        resources: {
+          requests: {
+            cpu: cpu,
+            memoryInGB: memory,
+          },
+        },
+      },
+    ],
+    ipAddress: {
+      type: containerinstance.ContainerGroupIpAddressType.Public,
+      dnsNameLabel: `${imageName}`,
+      ports: [
+        {
+          port: publicPort,
+          protocol: 'tcp',
+        },
+      ],
+    },
+  },
+)
+
+// Export the service's IP address, hostname, and fully-qualified URL.
+export const hostname = containerGroup.ipAddress.apply((addr) => addr!.fqdn!)
+export const ip = containerGroup.ipAddress.apply((addr) => addr!.ip!)
+export const url = containerGroup.ipAddress.apply(
+  (addr) => `http://${addr!.fqdn!}:${containerPort}`,
+)
